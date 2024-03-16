4c4bf80e8ff64ba9120def0ed0fa1e7edc42a222
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-03-15 20:05:22 -0500

    feat: :art: improve code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,11 +7,17 @@ export default {
 				'light-pink': 'hsl(275, 100%, 97%)',
 				'grayish-purple': 'hsl(292, 16%, 49%)',
 				'dark-purple': 'hsl(292, 42%, 14%)',
+				'purple': 'hsl(281, 83%, 54%)',
 			},
 			colors: {
 				'light-pink': 'hsl(275, 100%, 97%)',
 				'grayish-purple': 'hsl(292, 16%, 49%)',
 				'dark-purple': 'hsl(292, 42%, 14%)',
+				'purple': 'hsl(281, 83%, 54%)',
+			},
+			backgroundImage: {
+				'pattern-desktop': "url('/images/background-pattern-desktop.svg')",
+				'pattern-mobile': "url('/images/background-pattern-mobile.svg')",
 			},
 		},
 	},
